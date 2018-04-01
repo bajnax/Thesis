c139fb03f94c22f88ed4637da1f81b1424154a1b
Bainak Dmitrii <mr.baynak@gmail.com>
2018-04-01 15:33:52 +0300

    Gas values also get saved in the Room database and observed by the ViewModel

---
+++ app/src/main/java/com/savonia/thesis/db/dao/GasDao.java
@@ -0,0 +1,24 @@
+package com.savonia.thesis.db.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.savonia.thesis.db.entity.Gas;
+
+import java.util.List;
+
+@Dao
+public interface GasDao {
+
+    @Insert
+    void insert(Gas gas);
+
+    @Query("DELETE FROM gas_table")
+    void deleteAll();
+
+    @Query("SELECT * from gas_table ORDER BY id")
+    LiveData<List<Gas>> getAllGasValues();
+
+}
